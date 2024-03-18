3d607c0f64d9c55d7fe35cd8355d51512024a4f5
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2024-03-18 16:34:49 -0400

    adicionados os valores da tabel tipo contrato

--- js/script.js
+++ js/script.js
@@ -11,15 +11,14 @@ document.getElementById("generatorForm").addEventListener("submit", function(eve
   // }
 
   let data = {
+    codUnidadeOrcamentaria: document.getElementById('codUnidadeOrcamentaria').value,
+		numContrato: document.getElementById('numContrato').value,
+		tpContrato: document.getElementById('tpContrato').value,
+    // tpContrato: tpContrato,
     cpf: document.getElementById('cpf').value.replace(/\D/g,''),
     matricula: document.getElementById('matricula').value,
-		
-    tpAto: tpAto,
-    numero: document.getElementById('numero').value,
-    dtAto: dtAto,
-    onus: document.getElementById('onus').value,
-    disposicaoEntidadePrivada: document.getElementById('disposicaoEntidadePrivada').value,
-    cnpjCedenteCessionario: cnpjCedenteCessionario.replace(/\D/g,'')
+    nome: document.getElementById('nome').value,
+    tpResponsavel: tpResponsavel,
   }
 
 	const dataLikeAnArray = [data]
@@ -28,43 +27,43 @@ document.getElementById("generatorForm").addEventListener("submit", function(eve
   let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(dataLikeAnArray));
   let downloadAnchorNode = document.createElement('a');
   downloadAnchorNode.setAttribute("href", dataStr);
-  downloadAnchorNode.setAttribute("download", "ATOCESSAODISPOSICAO.json");
+  downloadAnchorNode.setAttribute("download", "RESPONSAVELCONTRATO.json");
   document.body.appendChild(downloadAnchorNode);
   downloadAnchorNode.click();
   downloadAnchorNode.remove();
 })
 
 
-function loadJsonFile(event) {
-  let file = event.target.files[0];
-  if (file) {
-    let reader = new FileReader();
-    reader.onload = function(e) {
-      let contents = e.target.result;
-      let json = JSON.parse(contents);
+// function loadJsonFile(event) {
+//   let file = event.target.files[0];
+//   if (file) {
+//     let reader = new FileReader();
+//     reader.onload = function(e) {
+//       let contents = e.target.result;
+//       let json = JSON.parse(contents);
 
-      if (Object.keys(json).length === 1) {
-        json = json[Object.keys(json)[0]];
-      }
+//       if (Object.keys(json).length === 1) {
+//         json = json[Object.keys(json)[0]];
+//       }
 
-      // Preencha os campos do formulário com os dados do arquivo JSON
-      document.getElementById('cpf').value = json.cpf;
-      document.getElementById('matricula').value = json.matricula;
+//       // Preencha os campos do formulário com os dados do arquivo JSON
+//       document.getElementById('cpf').value = json.cpf;
+//       document.getElementById('matricula').value = json.matricula;
 
-      document.getElementById('tpAto').value = json.tpAto;
-      document.getElementById('numero').value = json.numero;
-      document.getElementById('dtAto').value = json.dtAto;
-      document.getElementById('onus').value = json.onus;
-      document.getElementById('disposicaoEntidadePrivada').value = json.disposicaoEntidadePrivada;
-      document.getElementById('cnpjCedenteCessionario').value = json.cnpjCedenteCessionario;
+//       document.getElementById('tpAto').value = json.tpAto;
+//       document.getElementById('numero').value = json.numero;
+//       document.getElementById('dtAto').value = json.dtAto;
+//       document.getElementById('onus').value = json.onus;
+//       document.getElementById('disposicaoEntidadePrivada').value = json.disposicaoEntidadePrivada;
+//       document.getElementById('cnpjCedenteCessionario').value = json.cnpjCedenteCessionario;
 
-      // Aplique as máscaras novamente
-      $('.cpf-mask').mask('000.000.000-00').trigger('input');
-    };
-    reader.readAsText(file);
-  }
-}
+//       // Aplique as máscaras novamente
+//       $('.cpf-mask').mask('000.000.000-00').trigger('input');
+//     };
+//     reader.readAsText(file);
+//   }
+// }
 
 
 
-document.getElementById('jsonFile').addEventListener('change', loadJsonFile);
+// document.getElementById('jsonFile').addEventListener('change', loadJsonFile);
